dec75e14c90c8b57a51cf083adb8b7ccc7bd70da
Ari Firmanto <arifirmanto@developer.laravel@gmail.com>
2013-09-13 20:58:49 +0700

    styling dashboard menu

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def flash_message
+    flash.inject('') do |message, element|
+      message = message + (content_tag :div, element.last.html_safe, :class=> "message", 
+        :id => "flash_#{element.first}") if (element.last.is_a?(String))
+      message.html_safe
+    end
+  end
 end
