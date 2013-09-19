95e5d840d21bd41e7decee7f00af4405938dd67c
Ari Firmanto <arifirmanto@developer.laravel@gmail.com>
2013-09-19 17:58:20 +0700

    moving dynamic params into static params

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,21 +1,13 @@
 class Order
   include ActiveModel::Model
 
-  attr_accessor :to, :subject, :cc, :content, :dynamic_params
+  attr_accessor :to, :subject, :cc, :content, :name, :address, :date
 
   validates_presence_of :to
   validates_format_of :to, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i,
     :message => "email format is wrong"
   validates_presence_of :content, :message => "upload email template first, 
                                                before sending an email"
-  #class method
-  def self.build_dynamic_method params
-    params.each do |method|
-      define_method("#{method}="){ |param| instance_variable_set("@#{method}", param) }
-      define_method(method){ instance_variable_get("@#{method}") }
-    end
-  end
-
   #instance method
   def initialize(attr={})
     #get the content first from email template
@@ -30,12 +22,11 @@ class Order
   #Read Json file and parse the content and build setter and getter
   def parse_parameter_in_file
     @dynamic_params = content.scan(/{.+}/).map{|s| s.gsub(/{/, '').gsub(/}/, '')}
-    Order.build_dynamic_method(dynamic_params)
   end
   
   #Replace predefined variable with new variable that user has inputed
   def get_new_content
-    dynamic_params.each do |p|
+    @dynamic_params.each do |p|
       content.gsub!(/{#{Regexp.escape(p)}}/, self.send(p.to_sym)) if self.send(p.to_sym).present?
     end
   end
